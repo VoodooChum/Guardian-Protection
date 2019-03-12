e0c39a7f3bcfa231d002ce577b287e22705a0edc
voodoochum <brian@aquavisionnola.com>
2019-03-12 13:11:10 -0500

    Changed Background Color

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.tsx to start working on your app!</Text>
       </View>
     );
   }
@@ -14,7 +14,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0078ef',
     alignItems: 'center',
     justifyContent: 'center',
   },
