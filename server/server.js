afc67dffb9c62e7dc3653214e6488258286bdb89
Akin <akinpds@gmail.com>
2019-03-13 16:02:23 -0500

    Required request handlers. Successfully add a user

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require("body-parser"); // Requiring body-parser to obtain the body from post requests
 const app = express();
 const port = process.env.PORT || 3000;
-
+const { createUser } = require('../db/helpers/request-handlers')
 // Set Express to use body-parser as a middleware //
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
