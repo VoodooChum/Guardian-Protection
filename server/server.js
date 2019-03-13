7280ed949eab2a4027d5d8de73ab0e9e954da9b0
voodoochum <brian@aquavisionnola.com>
2019-03-13 11:56:58 -0500

    Signup route handles post request and responds with the body and 201

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 require('dotenv'); // this allows us to use the process variables
 const express = require('express');
-
+const bodyParser = require("body-parser"); // Requiring body-parser to obtain the body from post requests
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).send('Yea this works');
-})
+// Set Express to use body-parser as a middleware //
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Yea this works");
+});
+
+app.post("/signup", (req, res) => {
+  const userInfo = req.body;
+  res.status(201).send(userInfo);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
