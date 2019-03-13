14dd213e24b3b49531f45e044e3c64e97315b6bd
voodoochum <brian@aquavisionnola.com>
2019-03-13 14:27:48 -0500

    Added bodyParser to dependencies and eslintrc file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config(); // this allows us to use the process variables
 const express = require('express');
-const bodyParser = require("body-parser"); // Requiring body-parser to obtain the body from post requests
+const bodyParser = require('body-parser'); // Requiring body-parser to obtain the body from post requests
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -8,11 +9,11 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.status(200).send("Yea this works");
+app.get('/', (req, res) => {
+  res.status(200).send('Yea this works');
 });
 
-app.post("/signup", (req, res) => {
+app.post('/signup', (req, res) => {
   const userInfo = req.body;
   res.status(201).send(userInfo);
 });
