cb1c09ecdf771f0b5ce98dc1bb5dcf5cd3fde72b
voodoochum <brian@aquavisionnola.com>
2019-03-15 19:44:12 -0500

    Added call to signup funtion at databas helpers

--- server/server.js
+++ server/server.js
@@ -42,11 +42,7 @@ app.get("/", (req, res) => {
 app.post('/login', passport.authenticate('local'), login); 
 
 
-app.post("/signup", (req, res) => {
-  const userInfo = req.body;
-  console.log(userInfo);
-  res.status(201).send(userInfo);
-});
+app.post("/signup", signup);
 
 app.post('/create', createUser);
 
