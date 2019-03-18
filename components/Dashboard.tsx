a52f19f2d481f2c17d56c5d78ff49fb0d2380337
voodoochum <brian@aquavisionnola.com>
2019-03-18 09:07:13 -0500

    Dashboard Panic Button triggering camera

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -59,9 +59,10 @@ class DashboardView extends React.Component{
   }
 
   onPressPanic = () => {
-    this.props.panic();
     // Do whatever you need here to switch to Joining a group View
     console.log('Panic Button Pressed');
+    this.props.panic();
+    
   }
  
   onPressViewGroup1 = () => {
