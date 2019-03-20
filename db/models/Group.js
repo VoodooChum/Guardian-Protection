d7560bbdc02511a3fc046512e5a5806dfc0b8091
voodoochum <brian@aquavisionnola.com>
2019-03-20 12:14:07 -0500

    Group Table requires id_chat.  Still commented out till we drop table

--- db/models/Group.js
+++ db/models/Group.js
@@ -2,7 +2,8 @@ module.exports = (sequelize, DataTypes) => {
     const Group = sequelize.define('Group', {
         id_user_creator: { type: DataTypes.INTEGER, unique: false, allowNull: false },
         name: { type: DataTypes.STRING, unique: true, allowNull: false },
-        passcode: { type: DataTypes.STRING, unique: false, allowNull: false}
+        passcode: { type: DataTypes.STRING, unique: false, allowNull: false},
+        // id_chat: { type: DataTypes.STRING, unique: true, allowNull: false }
     });
 
     Group.associate = (models) => {
