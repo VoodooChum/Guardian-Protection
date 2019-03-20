7008bd409337304867f89721fc44bfbd0a7fdd2d
Michael LeMaire Jr <spark.michael.lemaire@gmail.com>
2019-03-20 15:17:50 -0500

    Fixes some new issues with the database

--- db/models/UserLocation.js
+++ db/models/UserLocation.js
@@ -1,7 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
     const UserLocation = sequelize.define('UserLocation', {
-        id_user: { type: DataTypes.INTEGER, unique: true, allowNull: false },
-        id_location: { type: DataTypes.INTEGER, unique: true, allowNull: false },
         time: { type: DataTypes.DATE, unique: false, allowNull: true },
     });
 
