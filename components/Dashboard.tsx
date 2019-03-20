afeb05c9009cb57c075dd3336f832d996bc2b566
voodoochum <brian@aquavisionnola.com>
2019-03-20 12:13:28 -0500

    Removed unused constants

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -35,13 +35,7 @@ class DashboardView extends React.Component{
     this._isMounted = false;
   }
 
-  // componentDidUpdate = async () => {
-  //   const CancelToken = axios.CancelToken;
-  //   const source = CancelToken.source();
-  //   let myGroups = await axios.get(`${API_HOST}/myGroups/${this.props.userData.id}`, {cancelToken: source.token});
-  //   this.setState({ groups: myGroups.data });
-  //   source.cancel('operationCanceled');
-  // };
+ 
 
   componentDidMount = () => {
     this._isMounted = true;
