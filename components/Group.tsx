e83194395dc20a4489739edff62516569024e065
voodoochum <brian@aquavisionnola.com>
2019-03-21 11:53:37 -0500

    Fix for Android View

--- components/Group.tsx
+++ components/Group.tsx
@@ -139,7 +139,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    width: 375,
+    // width: 375, removing for auto sizing
     marginTop: 0,
     padding: 30,
     borderRadius: 8,
