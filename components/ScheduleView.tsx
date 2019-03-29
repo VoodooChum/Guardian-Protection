10c166006a7165ae2edff8f2301e88274e2faafd
Michael LeMaire Jr <spark.michael.lemaire@gmail.com>
2019-03-28 21:08:35 -0500

    Adds map to schedule view

--- components/ScheduleView.tsx
+++ components/ScheduleView.tsx
@@ -2,15 +2,35 @@ import * as React from 'react';
 import { Text, View, TouchableOpacity, Button } from 'react-native';
 import axios from 'axios';
 import { createStackNavigator, createAppContainer, withNavigation } from 'react-navigation';
-
+import { MapView } from 'expo';
+// const {API_HOST} = Constants.manifest.extra;
+const API_HOST = 'http://73376243.ngrok.io';
 class Schedule extends React.Component {
   constructor(props:object){
     super(props);
+    console.log(this.props.navigation.state.params);
+  }
+  async getMarkers(){
+    try {
+      const { data } = await axios.get(`${API_HOST}//schedule/retrieve/${this.props.navigation.state.params.userInfo.id}`);
+      console.log(data);
+    } catch(e){
+      console.log(e);
+    }
   }
   render(){
+    const memberLocation = this.props.navigation.state.params.location
     return (
-      <Text>This component does exist</Text>
+      <MapView 
+          style={{ flex: 1 }}
+          initialRegion={{
+          latitude: parseFloat(memberLocation.latitude),
+          longitude: parseFloat(memberLocation.longitude),
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      ></MapView>
     )
   }
 }
-export default Schedule;
+export default withNavigation(Schedule);
