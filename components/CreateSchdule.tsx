bc30741be6d0ad93fb3545bb22eaf6077e8ba076
Michael LeMaire Jr <spark.michael.lemaire@gmail.com>
2019-03-28 19:05:12 -0500

    Adds CreateSchedule view

---
+++ components/CreateSchdule.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Text, View, TouchableOpacity, Button } from 'react-native';
+import axios from 'axios';
+import { createStackNavigator, createAppContainer, withNavigation } from 'react-navigation';
+
+class CreateSchedule extends React.Component {
+  constructor(props: object) {
+    super(props);
+  }
+  render() {
+    return (
+      <Text>This component does exist</Text>
+    )
+  }
+}
+export default CreateSchedule;
