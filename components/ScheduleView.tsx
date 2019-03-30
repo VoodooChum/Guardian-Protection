515aab4e585f461e72362a7e9bf56b5ccd66602f
Michael LeMaire Jr <spark.michael.lemaire@gmail.com>
2019-03-30 15:11:24 -0500

    Adds Map to Schedule View

--- components/ScheduleView.tsx
+++ components/ScheduleView.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { Text, View, TouchableOpacity, Button } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 import axios from 'axios';
+import { ThemeProvider, Button, Icon } from "react-native-elements";
 import { createStackNavigator, createAppContainer, withNavigation } from 'react-navigation';
-import { MapView } from 'expo';
-// const {API_HOST} = Constants.manifest.extra;
-const API_HOST = 'https://56bf53d3.ngrok.io';
+import { MapView, Constants } from 'expo';
+const {API_HOST} = Constants.manifest.extra;
+// const API_HOST = 'http://60c7f24d.ngrok.io';
 class Schedule extends React.Component {
   constructor(props:object){
     super(props);
@@ -23,15 +24,15 @@ class Schedule extends React.Component {
   render(){
     const memberLocation = this.props.navigation.state.params.location
     return (
-      <MapView 
-          style={{ flex: 1 }}
-          initialRegion={{
-          latitude: parseFloat(memberLocation.latitude),
-          longitude: parseFloat(memberLocation.longitude),
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      ></MapView>
+        <MapView 
+            style={{ flex: 1 }}
+            initialRegion={{
+            latitude: parseFloat(memberLocation.latitude),
+            longitude: parseFloat(memberLocation.longitude),
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}>
+        </MapView>
     )
   }
 }
