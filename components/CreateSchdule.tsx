fde00b15977966ad870cb425950005836febc560
Michael LeMaire Jr <spark.michael.lemaire@gmail.com>
2019-03-30 15:11:03 -0500

    Full functionality to create schedule

--- components/CreateSchdule.tsx
+++ components/CreateSchdule.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
-import { Text, View, TouchableOpacity, Button } from 'react-native';
+import { Text, View, TouchableOpacity, Button, StyleSheet } from 'react-native';
 import axios from 'axios';
 import { createStackNavigator, createAppContainer, withNavigation } from 'react-navigation';
 import t from 'tcomb-form-native';
+import {Location, Constants} from 'expo'
+const { API_HOST } = Constants.manifest.extra;
+// const API_HOST = 'http://60c7f24d.ngrok.io';
 const Form = t.form.Form;
 const options = {
   auto: "placeholders",
@@ -14,18 +17,46 @@ const Schedule = t.struct({
 class CreateSchedule extends React.Component {
   constructor(props: object) {
     super(props);
+    this.onPressCreateSchedule = this.onPressCreateSchedule.bind(this);
   }
-  onPressCreateSchedule(){
-
+  async onPressCreateSchedule(){
+    const value = this.refs.form.getValue();
+    try {
+      const location = await Location.geocodeAsync(value.address);
+      console.log(location);
+      console.log(this.props.navigation.state.params.userInfo);
+      const body = {
+        latitude: location[0].latitude.toString(),
+        longitude: location[0].longitude.toString(),
+        userId: this.props.navigation.state.params.userInfo.id
+      }
+      try{
+        const addEvent = await axios.post(`${API_HOST}/schedule/create`, body);
+        if(addEvent){
+          this.props.navigation.navigate('Dashboard', {
+              userData: this.props.navigation.state.params.userInfo,
+              name: this.props.navigation.state.params.name
+          })
+        } else {
+          console.log(addEvent);
+        }
+      } catch(e){
+        console.log(e.message)
+      }
+    } catch(e){
+      console.log(e.message);
+    }
   }
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Form ref="form" type={Schedule} options={options} />
         <TouchableOpacity
         style={styles.button}
         onPress={this.onPressCreateSchedule}
-        ></TouchableOpacity>
+        >
+        <Text>Create Event</Text>
+        </TouchableOpacity>
       </View>
     )
   }
@@ -62,4 +93,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default CreateSchedule;
+export default withNavigation(CreateSchedule);
